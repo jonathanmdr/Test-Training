d083305ebbf1d6406a648eeb48dcd55579e45f10
Jonathan Henrique <jonathan.medeiros@objective.com.br>
2020-09-11 16:16:30 -0300

    Adjustment exception class

--- src/main/java/br/com/objective/exercices/utils/exception/LetterDictionaryException.java
+++ src/main/java/br/com/objective/exercices/utils/exception/LetterDictionaryException.java
@@ -2,10 +2,6 @@ package br.com.objective.exercices.utils.exception;
 
 public class LetterDictionaryException extends RuntimeException {
 
-    public LetterDictionaryException(String message) {
-        super(message);
-    }
-
     public LetterDictionaryException(String message, Exception ex) {
         super(message, ex);
     }
